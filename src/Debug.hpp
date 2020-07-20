2c959c205bba2c04dee3579f10ce93718142f877
Copeland <copeland@205-185-99-26.directcom.com>
2020-07-19 21:04:52 -0600

    not sure what this is...

--- src/Debug.hpp
+++ src/Debug.hpp
@@ -24,55 +24,55 @@ public:
     Debug() {};
 
     void logger(std::string message, unsigned int verbosity = verbose){
-        if(verbosity <= verbose)
-            std::cout << ++debugCount << ": " << message << std::endl;
+        // if(verbosity <= verbose)
+        //     std::cout << ++debugCount << ": " << message << std::endl;
     }
 
     void printVector(const std::vector<float>& v, int itemsPerLine = -1, int spacing = 10){
-        std::cout << "\n";
-        int count = 1;
-        for (auto i: v){
-            if ((count % itemsPerLine) or itemsPerLine == -1){
-                std::cout << std::setw(spacing) << i << ", ";
-                ++count;
-            }
-            else{
-                std::cout << std::setw(spacing) << i << "\n";
-                ++count;
-            }
-        }
-        std::cout << "\n";
+        // std::cout << "\n";
+        // int count = 1;
+        // for (auto i: v){
+        //     if ((count % itemsPerLine) or itemsPerLine == -1){
+        //         std::cout << std::setw(spacing) << i << ", ";
+        //         ++count;
+        //     }
+        //     else{
+        //         std::cout << std::setw(spacing) << i << "\n";
+        //         ++count;
+        //     }
+        // }
+        // std::cout << "\n";
     }
 
     void printVector(const std::vector<int>& v, int itemsPerLine = -1, int spacing = 10){
-        std::cout << "\n";
-        int count = 1;
-        for (auto i: v){
-            if ((count % itemsPerLine) or itemsPerLine == -1){
-                std::cout << std::setw(spacing) << i << ", ";
-                ++count;
-            }
-            else{
-                std::cout << std::setw(spacing) << i << "\n";
-                ++count;
-            }
-        }
-        std::cout << "\n";
+        // std::cout << "\n";
+        // int count = 1;
+        // for (auto i: v){
+        //     if ((count % itemsPerLine) or itemsPerLine == -1){
+        //         std::cout << std::setw(spacing) << i << ", ";
+        //         ++count;
+        //     }
+        //     else{
+        //         std::cout << std::setw(spacing) << i << "\n";
+        //         ++count;
+        //     }
+        // }
+        // std::cout << "\n";
     }
 
     void printVector(const std::vector<Point>& v, int itemsPerLine = -1, int spacing = 10){
-        std::cout << "\n";
-        int count = 1;
-        for (auto i: v){
-            if ((count % itemsPerLine) or itemsPerLine == -1){
-                std::cout << '(' << i.x << ", " << i.y << "), ";
-                ++count;
-            }
-            else{
-                std::cout << '(' << i.x << ", " << i.y << ")\n";
-                ++count;
-            }
-        }
-        std::cout << "\n";
+        // std::cout << "\n";
+        // int count = 1;
+        // for (auto i: v){
+        //     if ((count % itemsPerLine) or itemsPerLine == -1){
+        //         std::cout << '(' << i.x << ", " << i.y << "), ";
+        //         ++count;
+        //     }
+        //     else{
+        //         std::cout << '(' << i.x << ", " << i.y << ")\n";
+        //         ++count;
+        //     }
+        // }
+        // std::cout << "\n";
     }
 };
